114ac9636603633f76313f66ea6bcec75f9b8f82
Alexander Poon <scribbleseq@gmail.com>
2016-04-30 14:06:26 -0400

    Layout and routes

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ source 'https://rubygems.org'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.6'
+# Bootstrap SASS
+gem 'bootstrap-sass'
 # Use postgres as the database for Active Record
 gem 'pg'
 # Use SCSS for stylesheets
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
-  get 'static_pages/help'
-  get 'static_pages/about'
+  get 'users/new'
+
+  root              'static_pages#home'
+  get  'help'    => 'static_pages#help'
+  get  'about'   => 'static_pages#about'
+  get  'contact' => 'static_pages#contact'
+  get  'signup'  => 'users#new'
 
   resources :microposts
   resources :users
