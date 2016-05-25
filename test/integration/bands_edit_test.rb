eedd48e2db99d6eaa041aec6c9c870dd7120f26e
Alexander Poon <scribbleseq@gmail.com>
2016-05-25 00:04:33 -0400

    Integration tests for editing bands and events.

---
+++ test/integration/bands_edit_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+
+class BandsEditTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:alexander)
+    @band = bands(:h4h)
+  end
+
+  test "unsuccessful edit" do
+    log_in_as(@user)
+    get edit_band_path(@band)
+    assert_template 'bands/edit'
+    patch band_path(@band), band: { name:  ""}
+    assert_template 'bands/edit'
+  end
+
+  test "successful edit with friendly forwarding" do
+    get edit_band_path(@band)
+    log_in_as(@user)
+    assert_redirected_to edit_band_path(@band)
+    name  = "The Left Behind"
+    patch band_path(@band), band: { name:  name }
+    assert_not flash.empty?
+    assert_redirected_to bands_user_path(@user)
+    @band.reload
+    assert_equal name,  @band.name
+  end
+end
---
+++ test/integration/events_edit_test.rb
@@ -0,0 +1,21 @@
+require 'test_helper'
+
+class EventsEditTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:alexander)
+    @event = events(:rebar)
+  end
+
+  test "successful edit with friendly forwarding" do
+    get edit_event_path(@event)
+    log_in_as(@user)
+    assert_redirected_to edit_event_path(@event)
+    name  = "Battle Rock Seattle at the Re-bar"
+    patch event_path(@event), event: {name: name}
+    assert_not flash.empty?
+    assert_redirected_to events_path
+    @event.reload
+    assert_equal name,  @event.name
+  end
+end
