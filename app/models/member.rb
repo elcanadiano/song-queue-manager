900c9172d53eef2e036ae6d2b25b9e605fbf4531
Alexander Poon <scribbleseq@gmail.com>
2016-07-03 17:11:30 -0400

    Uniqueness constraint for members on User and Band IDs.

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,4 +1,10 @@
 class Member < ActiveRecord::Base
+  validates_each :user_id, :band_id  do |record, attr, value|
+    if Member.exists?(user_id: record.user_id, band_id: record.band_id)
+        record.errors.add attr, 'This user is already part of the band.'
+    end
+  end
+
   belongs_to :user
   belongs_to :band
 
--- test/models/member_test.rb
+++ test/models/member_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class MemberTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @dankey = users(:dankey)
+    @tribe  = bands(:dankey)
+    @lana   = users(:lana)
+  end
+
+  test "should be valid" do
+    m = Member.new(user_id: @lana.id, band_id: @tribe.id)
+    assert m.valid?
+  end
+
+  test "there should not be a duplicate member" do
+    m = Member.new(user_id: @dankey.id, band_id: @tribe.id)
+    assert_not m.valid?
+  end
 end
