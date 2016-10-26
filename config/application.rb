cad1275231d73ed5753d6c13be352188fe7726bc
Alexander Poon <scribbleseq@gmail.com>
2016-10-26 19:43:23 -0400

    Removed scaffolds, changed song count to bootstrap table.

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,10 @@ module TestApp
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    config.generators do |g|
+      g.stylesheets false
+    end
+
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
