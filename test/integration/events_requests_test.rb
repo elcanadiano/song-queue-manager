797d08e572382d9396478d82ff5b4ea4d53fbc86
Alexander Poon <scribbleseq@gmail.com>
2016-11-04 15:37:57 -0400

    Requests count has message if no requests were completed, new test for this.

--- test/integration/events_requests_test.rb
+++ test/integration/events_requests_test.rb
@@ -3,8 +3,9 @@ require 'test_helper'
 class EventsRequestsTest < ActionDispatch::IntegrationTest
 
   def setup
-    @event = events(:brs8)
-    @admin = users(:alexander)
+    @event                         = events(:brs8)
+    @event_with_no_completed_songs = events(:brs7)
+    @admin                         = users(:alexander)
   end
 
   # Tests for the song count sidebar of brs8 according to the fixture data.
@@ -14,10 +15,11 @@ class EventsRequestsTest < ActionDispatch::IntegrationTest
   # Dankey Kang and the Tribe - 3
   # Harmonies for Hire        - 3
   # No Boston After Midnight  - 2
-  test "song requests count for ongoing event" do
+  test "song requests count for ongoing event with completed songs" do
     log_in_as(@admin)
     get event_path(@event)
     assert_template 'events/show'
+
     # Assert the table exists.
     assert_select "#song-request-count > tbody > tr", 3
 
@@ -32,5 +34,20 @@ class EventsRequestsTest < ActionDispatch::IntegrationTest
     # NBAM is last.
     assert_select "#song-request-count > tbody > tr:nth-child(3) > td:nth-child(1)", "No Boston After Midnight"
     assert_select "#song-request-count > tbody > tr:nth-child(3) > td:nth-child(2)", "2"
+  end
+
+  # BRS7 has no completed song requests, so there should not be a requests
+  # table. Instead we get a message saying, "There have been no completed
+  # songs."
+  test "no song requests count for ongoing event without completed songs" do
+    log_in_as(@admin)
+    get event_path(@event_with_no_completed_songs)
+    assert_template 'events/show'
+
+    # Assert the table does not exist.
+    assert_select "#song-request-count", false, "The Song Request Table should not exist."
+
+    # Assert that request-sidebar contains the message.
+    assert_select "#request-sidebar", "There have been no completed songs."
   end
 end
